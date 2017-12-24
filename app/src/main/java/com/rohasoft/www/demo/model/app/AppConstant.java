e234999407c4016b949da3ebc208e02865f122e0
Kris <krishnamanieee@gmail.com>
2017-12-24 08:23:48 +0530

    Initial commit

---
+++ app/src/main/java/com/rohasoft/www/demo/model/app/AppConstant.java
@@ -0,0 +1,12 @@
+package com.rohasoft.www.demo.model.app;
+
+/**
+ * Created by krish on 12/23/2017.
+ */
+
+public class AppConstant {
+
+
+    // Main domain
+    public static final String DOMAIN="http://www.rohasoft.com/";
+}
